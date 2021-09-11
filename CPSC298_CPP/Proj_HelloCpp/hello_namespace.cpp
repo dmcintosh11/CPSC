272687c57c071f73b02c1fa177b02bc8a358cdfa
dmcintosh11 <dmcintosh@chapman.edu>
2021-09-11 23:59:32 +0000

    Proj_HelloCpp hello with namespace

---
+++ CPSC298_CPP/Proj_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,9 @@
+/// @file hello_namespace.cpp
+/// @brief Outputs Hello C++! and uses a namespace.
+/// @author Dylan McIntosh
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello C++!" << endl;
+  return 0;
+}
