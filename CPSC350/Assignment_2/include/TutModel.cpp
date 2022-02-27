7da4c66f527b4c2ca08c8688bccd039567e5a96a
dmcintosh11 <dmcintosh@chapman.edu>
2022-02-27 06:04:02 +0000

    Updates to functional files

--- CPSC350/Assignment_2/include/TutModel.cpp
+++ CPSC350/Assignment_2/include/TutModel.cpp
@@ -1,6 +1,7 @@
 //Class to translate letters in English to the correspondnding Tutnese phrase
 
 #include "TutModel.h"
+#include <iostream>
 
   TutModel::TutModel() {
 
@@ -10,28 +11,35 @@
 
   }
 
+  //Takes a string as input and uses Model.cpp methods to find the mathcing character in English to Tutnese phrases and outputs translation from Tutnese to English
   std::string TutModel::translatePhrase(std::string s) {
     std::string translation = "";
     for(char c = 'A'; c <= 'z'; ++c) {
+
       if(s.length() > 0) {
         std::string test = m.translateSingleCharacter(c);
-        if(s.find(test) == 0) {
-          translation += c;
-          s = s.substr(test.length() - 1);
+        if(s.at(0) == ' ' || ispunct(s.at(0))) {
+          translation += s.at(0);
+          s = s.substr(1);
           c = 'A' - 1;
-        } else {
-          std::string testDouble = m.translateDoubleCharacter(c);
-          if(s.find(testDouble) == 0) {
-            translation += (c + c);
-            s = s.substr(testDouble.length() - 1);
+        }
+          else if(s.find(test) == 0) {
+            translation += c;
+            s = s.substr(test.length());
             c = 'A' - 1;
+          } else {
+            std::string testDouble = m.translateDoubleCharacter(c);
+            if(s.find(testDouble) == 0) {
+              translation += c;
+              translation += c;
+              s = s.substr(testDouble.length());
+              c = 'A' - 1;
 
           }
         }
 
       }
 
-      return translation;
     }
 
     return translation;
--- CPSC350/Assignment_2/include/main.cpp
+++ CPSC350/Assignment_2/include/main.cpp
@@ -5,16 +5,20 @@
 
 int main(int argc, char** argv) {
 
+
   std::string input = argv[1];
   std::string output = argv[2];
   std::string lang = argv[3];
 
+
   if(lang.compare("E2T") == 0) {
     FileProcessor f;
     f.processFile(input, output);
-  } else {
+  } else if(lang.compare("T2E") == 0){
     TutFileProcessor f;
     f.processFile(input, output);
+  } else {
+    std::cout << "Specify \"E2T\" or \"T2E\" as the third command line argument" << std::endl;
   }
 
 
