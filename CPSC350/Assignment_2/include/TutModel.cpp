1ce3e5c3815fd40a1c06d937601bc1635bdbaaf3
Dylan McIntosh <69490215+dmcintosh11@users.noreply.github.com>
2022-02-26 23:22:46 -0800

    Update TutModel.cpp

--- CPSC350/Assignment_2/include/TutModel.cpp
+++ CPSC350/Assignment_2/include/TutModel.cpp
@@ -18,16 +18,20 @@
 
       if(s.length() > 0) {
         std::string test = m.translateSingleCharacter(c);
+        //Checks to see if string 's' starts with a space or punctuation
         if(s.at(0) == ' ' || ispunct(s.at(0))) {
           translation += s.at(0);
           s = s.substr(1);
           c = 'A' - 1;
         }
+          //Checks to see if the English to Tut translation of char 'c' is at the beginning of string 's'
           else if(s.find(test) == 0) {
-            translation += c;
-            s = s.substr(test.length());
-            c = 'A' - 1;
-          } else {
+            translation += c; //Adds English translation of phrase to string 'translation'
+            s = s.substr(test.length()); //Shortens string 's' to everything after the phrase that was just translated
+            c = 'A' - 1; //Resets for loop to go back through the alphabet
+          } 
+          //Checks to see if the next phrase is a double letter translation and if so, appends the English characters to the end of string 'translation'
+          else {
             std::string testDouble = m.translateDoubleCharacter(c);
             if(s.find(testDouble) == 0) {
               translation += c;
